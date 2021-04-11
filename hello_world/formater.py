d6b17eefa6e185ed5474b83487cafa0d2032589d
szymonsli <szymonsli@users.noreply.github.com>
2021-04-11 11:44:42 +0200

    Added xml support

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -1,10 +1,12 @@
+import json
 
 PLAIN = "plain"
 PLAIN_UP = "plain_uppercase"
 PLAIN_LO = "plain_lowercase"
 JSON = "json"
+XML = "xml"
 
-SUPPORTED = [PLAIN, PLAIN_UP, PLAIN_LO, JSON]
+SUPPORTED = [PLAIN, PLAIN_UP, PLAIN_LO, JSON, XML]
 
 
 def get_formatted(msg, imie, format):
@@ -17,12 +19,18 @@ def get_formatted(msg, imie, format):
         result = plain_text_lower_case(msg, imie)
     elif format == JSON:
         result = format_to_json(msg, imie)
+    elif format == XML:
+        result = format_to_xml(msg, imie)
     return result
 
 
 def format_to_json(msg, imie):
-    return ('{ "imie":"' + imie + '", "mgs":' +
-            msg + '"}')
+    data = {"imie": imie, "msg": msg}
+    return json.dumps(data)
+
+
+def format_to_xml(msg, imie):
+    return ('<greetings><imie>' + imie + '</imie><msg>' + msg + '</msg></greetings>')  # noqa
 
 
 def plain_text(msg, imie):
--- test/test_views.py
+++ test/test_views.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 from hello_world import app
 from hello_world.formater import SUPPORTED
 
@@ -14,5 +15,15 @@ class FlaskrTestCase(unittest.TestCase):
         ','.join(SUPPORTED) in s
 
     def test_msg_with_output(self):
+        test_data = {"imie": "Szymon", "msg": "Hello World!"}
         rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"Szymon", "mgs":Hello World!"}', rv.data)
+        js = json.loads(rv.data)
+        self.assertEqual(test_data['msg'], js['msg'])
+        self.assertEqual(test_data['imie'], js['imie'])
+
+    def test_msg_with_xml_output(self):
+        rv = self.app.get('/?output=xml')
+        s = b'<greetings><imie>Szymon</imie><msg>Hello World!</msg></greetings>'  # noqa
+        self.assertEqual(
+            s,
+            rv.data)
